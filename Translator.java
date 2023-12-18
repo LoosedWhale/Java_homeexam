dec5776c9df455bb964ad38225e9e69d1b2243b3
LoosedWhale <81869656+LoosedWhale@users.noreply.github.com>
2023-12-18 15:35:18 +0100

    Update Translator.java

--- Translator.java
+++ Translator.java
@@ -1,76 +1,56 @@
 package Translator;
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
+import java.util.*;
 import java.util.HashMap;
-import java.awt.event.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
 import java.util.Map;
 
 public class Translator {
-	private static String languageChoice = "English"; // Default language choice
+	// Sets a default language 
+	private static String languageChoice = "English";
 	public static void main(String[] args) {
 		
-		
-	// Initialize the list to store words
+		// Initialize the list to store words
         List<Map<String, String>> wordList = new ArrayList<>();
-
-	// List of words 
-        addWord(wordList, "Hello", "Hola", "Ciao", "Hej");
-        addWord(wordList, "Goodbye", "Adiós", "Arrivederci", "Adjö");
-        addWord(wordList, "Thank you", "Gracias", "Grazie", "Tack");
-    	addWord(wordList, "Yes", "Sí", "Sì", "Ja");
-    	addWord(wordList, "No", "No", "No", "Nej");
-    	addWord(wordList, "Cat", "Gato", "Gatto", "Katt");
-    	addWord(wordList, "Dog", "Perro", "Cane", "Hund");
-    	addWord(wordList, "House", "Casa", "Casa", "Hus");
-    	addWord(wordList, "Car", "Coche", "Auto", "Bil");
-    	addWord(wordList, "Computer", "Computadora", "Computer", "Dator");
-
-
-
-		
-		
-		System.out.println(""
-				+ "   ____   _   _   ___ \r\n"
-				+ "  / ___| | | | | |_ _|\r\n"
-				+ " | |  _  | | | |  | | \r\n"
-				+ " | |_| | | |_| |  | | \r\n"
-				+ "  \\____|  \\___/  |___|\r\n"
-				+ "                      "
-				+ "");
-		
-		
-		
-		
+        initializeWordList(wordList);	
 		
 		//Creates a window / frame
-		JFrame f = new JFrame();
+		JFrame f = new JFrame("Top notch translator 3000");
+		f.getContentPane().setBackground(new Color(0, 100, 255));
+		
+		//Creates text area for print and label
+		final TextField print_txt = new TextField();
+		print_txt.setBounds(175,80,150,20);  
+		print_txt.setEditable(false);
+		
+		Label print_txtLabel = new Label("Translated word");
+		print_txtLabel.setBounds(175,60,150,20); 
+		print_txtLabel.setFont(new Font("Arial", Font.PLAIN, 12));
 
 		
-		//Creates text area for print
-		final TextField print_txt = new TextField();
-		print_txt.setBounds(175,80,150,20); //x, y, w, h 
-		
-		//Creates text area for input 
+		//Creates text area for input and label
 		final TextField input_txt = new TextField();
-		input_txt.setBounds(175, 20, 150, 20); //x, y, w, h
+		input_txt.setBounds(175, 20, 150, 20); 
 		input_txt.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				print_txt.setText(null);
 				String store_word_input = input_txt.getText();
 				String translation = getTranslation(wordList, store_word_input, languageChoice);
 				print_txt.setText(translation);
-				System.out.println("Translation: " + translation);	// debug
 				input_txt.setText(null);
 				}
 			});
 		
+		Label input_txtLabel = new Label("Input the word you want to translate");
+		input_txtLabel.setBounds(155, 0, 200, 20);
+		input_txtLabel.setFont(new Font("Arial", Font.PLAIN, 12));
+		
 		//Creates button for translation
 		Button translate_btn = new Button("Translate");
-		translate_btn.setBounds(150, 120, 200, 65); //x, y, w, h
+		translate_btn.setBounds(150, 120, 200, 65);
 		translate_btn.addActionListener(new ActionListener() 
 		{
 		public void actionPerformed(ActionEvent e) {
@@ -78,90 +58,61 @@ public class Translator {
 			String store_word_input = input_txt.getText();
 			String translation = getTranslation(wordList, store_word_input, languageChoice);
 			print_txt.setText(translation);
-			System.out.println("Translation: " + translation);	// debug
-					
-			}
-		});
-		
-		
-		//Creates button for English translation
-		Button english_btn = new Button("English");
-		english_btn.setBounds(20, 200, 100, 60); //x, y, w, h
-		english_btn.addActionListener(new ActionListener() 
-		{
-		public void actionPerformed(ActionEvent e) {
-			print_txt.setText(null);
-			String store_word_input = input_txt.getText();
-			String translation = getTranslation(wordList, store_word_input, "English");
-			print_txt.setText(translation);
-			System.out.println("Translation: " + translation); //debug
-			}
-		});
-		
-		//Creates button for Spanish translation
-		Button spanish_btn = new Button("Spanish");
-		spanish_btn.setBounds(380, 200, 100, 60); //x, y, w, h
-		spanish_btn.addActionListener(new ActionListener() 
-		{
-		public void actionPerformed(ActionEvent e) {
-			print_txt.setText(null);
-			String store_word_input = input_txt.getText();
-			String translation = getTranslation(wordList, store_word_input, "Spanish");
-			print_txt.setText(translation);
-			System.out.println("Translation: " + translation); //debug
-			}
-		});
-		
-		
-		//Creates button for Italian translation
-		Button italian_btn = new Button("Italian");
-		italian_btn.setBounds(260, 200, 100, 60); //x, y, w, h
-		italian_btn.addActionListener(new ActionListener() 
-		{
-		public void actionPerformed(ActionEvent e) {
-			print_txt.setText(null);
-			String store_word_input = input_txt.getText();
-			String translation = getTranslation(wordList, store_word_input, "Italian");
-			print_txt.setText(translation);
-			System.out.println("Translation: " + translation); //debug
-					
-			}
-		});
-		
-		//Creates button for Swedish translation
-		Button swedish_btn = new Button("Swedish");
-		swedish_btn.setBounds(140, 200, 100, 60); //x, y, w, h
-		swedish_btn.addActionListener(new ActionListener() 
-		{
-		public void actionPerformed(ActionEvent e) {
-			print_txt.setText(null);
-			String store_word_input = input_txt.getText();
-			String translation = getTranslation(wordList, store_word_input, "Swedish");
-			print_txt.setText(translation);
-			System.out.println("Translation: " + translation); //debug
-			
-					
 			}
 		});
 				
-		
-		
-        f.add(english_btn);
-		f.add(spanish_btn);
-		f.add(italian_btn);
-		f.add(swedish_btn);	
-		f.add(input_txt);
-		f.add(translate_btn);
-		f.add(print_txt);
-		f.setSize(515, 500);
-		f.setLayout(null);
-		f.setVisible(true);
+		// Creates translation language buttons
+        createTranslationButtons(f, wordList, input_txt, print_txt);
+	
+        f.add(input_txt);
+        f.add(input_txtLabel);
+        f.add(translate_btn);
+        f.add(print_txt);
+        f.add(print_txtLabel);
+        f.setSize(515, 500);
+        f.setLayout(null);
+        f.setVisible(true);
 		
 	}
 	
-	// Method to add a word and its translations to the list
+	
+	// Method to create translation language buttons
+    private static void createTranslationButtons(JFrame frame, List<Map<String, String>> wordList, TextField input_txt, TextField print_txt) {
+        String[] languages = {"English", "Spanish", "Italian", "Swedish"};
+
+        for (int i = 0; i < languages.length; i++) {
+            String language = languages[i];
+            Button languageBtn = new Button(language);
+            languageBtn.setBounds(20 + i * 120, 200, 100, 60);
+            languageBtn.addActionListener(new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    print_txt.setText(null);
+                    String store_word_input = input_txt.getText();
+                    String translation = getTranslation(wordList, store_word_input, language);
+                    print_txt.setText(translation);
+                }
+            });
+
+            frame.add(languageBtn);
+        }
+    }
+
+	
+	// Method to add a word and its translations to the list via looping trough a bunch
+    private static void initializeWordList(List<Map<String, String>> wordList) {
+        String[] englishWords = {"Hello", "Goodbye", "Thank you", "Yes", "No", "Cat", "Dog", "House", "Car", "Computer"};
+        String[] spanishWords = {"Hola", "Adiós", "Gracias", "Sí", "No", "Gato", "Perro", "Casa", "Coche", "Computadora"};
+        String[] italianWords = {"Ciao", "Arrivederci", "Grazie", "Sì", "No", "Gatto", "Cane", "Casa", "Auto", "Computer"};
+        String[] swedishWords = {"Hej", "Adjö", "Tack", "Ja", "Nej", "Katt", "Hund", "Hus", "Bil", "Dator"};
+
+        for (int i = 0; i < englishWords.length; i++) {
+            addWord(wordList, englishWords[i], spanishWords[i], italianWords[i], swedishWords[i]);
+        }
+    }
+	
+    // Method to add a word and its translations to the list
     private static void addWord(List<Map<String, String>> wordList, String english, String spanish, String italian, String swedish) {
-        Map<String, String> wordMap = new HashMap<>();
+    	Map<String, String> wordMap = new HashMap<>();
         wordMap.put("English", english);
         wordMap.put("Spanish", spanish);
         wordMap.put("Italian", italian);
